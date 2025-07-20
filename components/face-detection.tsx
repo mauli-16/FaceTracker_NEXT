b5676e88c23c7fb542e77f952d8708fcc410f6a3
Mauli Saxena <maulisaxena.16@gmail.com>
2025-07-20 19:20:44 +0530

    facial expressions and emotions

--- components/face-detection.tsx
+++ components/face-detection.tsx
@@ -2,7 +2,7 @@
 
 import { useRef, useEffect } from "react";
 import * as faceapi from "face-api.js";
-import { loadModels } from '../lib/faceDetection';
+import { loadModels } from "../lib/faceDetection";
 
 export default function FaceDetection() {
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -27,23 +27,26 @@ export default function FaceDetection() {
 
   useEffect(() => {
     const detect = async () => {
-      if (videoRef.current && canvasRef.current){
-        const video=videoRef.current
-        const canvas=canvasRef.current
-        const displaysize={
-          width:video.videoWidth,
-          height:video.videoHeight
-        }
-        faceapi.matchDimensions(canvas,displaysize)
-        const result = await faceapi.detectAllFaces(
-          video,
-          new faceapi.TinyFaceDetectorOptions()
-        );
-        const resizedDetections=faceapi.resizeResults(result,displaysize)
+      if (videoRef.current && canvasRef.current) {
+        const video = videoRef.current;
+        const canvas = canvasRef.current;
+        const displaysize = {
+          width: video.videoWidth,
+          height: video.videoHeight,
+        };
+        faceapi.matchDimensions(canvas, displaysize);
+        const result = await faceapi
+          .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+          .withFaceLandmarks()
+          .withFaceExpressions();
 
-        canvas.getContext("2d")?.clearRect(0,0,canvas.width,canvas.height);
-        faceapi.draw.drawDetections(canvas,resizedDetections)
-        
+        const resizedDetections = faceapi.resizeResults(result, displaysize);
+
+        canvas.getContext("2d")?.clearRect(0, 0, canvas.width, canvas.height);
+
+        faceapi.draw.drawDetections(canvas, resizedDetections);
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
 
         //nsole.log("Detection Result:", result);
       }
@@ -57,17 +60,14 @@ export default function FaceDetection() {
   }, []);
 
   return (
-     <div className="relative w-[500px] mt-4">
+    <div className="relative w-[500px] mt-4">
       <video
         ref={videoRef}
         autoPlay
         playsInline
         className="rounded shadow-lg w-full"
       />
-      <canvas
-        ref={canvasRef}
-        className="absolute top-0 left-0 w-full h-full"
-      />
+      <canvas ref={canvasRef} className="absolute top-0 left-0 w-full h-full" />
     </div>
   );
 }
