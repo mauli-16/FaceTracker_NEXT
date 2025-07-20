653c323cafd55e7e26440464668818190447fcc8
Mauli Saxena <maulisaxena.16@gmail.com>
2025-07-20 16:51:00 +0530

    canvas overlay

--- components/face-detection.tsx
+++ components/face-detection.tsx
@@ -1,31 +1,71 @@
-"use client"
+"use client";
 
-import { useRef,useEffect } from "react"
- export default function FaceDetection(){
-    const videoRef = useRef<HTMLVideoElement>(null);
-    useEffect(()=>{
-        //requesting webcam access
-        navigator.mediaDevices
-        .getUserMedia({video:true})//get user media available only on client = use client
-        .then((stream)=>{
-            if(videoRef.current){
-                videoRef.current.srcObject=stream //sets src obj of video dom element to  stream
-                //telling the video cam to play this feed
-               
-            }
+import { useRef, useEffect } from "react";
+import * as faceapi from "face-api.js";
+import { loadModels } from '../lib/faceDetection';
+
+export default function FaceDetection() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    // Load face-api models
+    loadModels().then(() => {
+      // Access webcam
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((stream) => {
+          if (videoRef.current) {
+            videoRef.current.srcObject = stream;
+          }
         })
-        .catch((err)=>{
-            console.error("Failed to access webcam:",err);
-        })
-    },[])
-     return (
-    <div className="flex flex-col items-center justify-center mt-4">
+        .catch((err) => {
+          console.error("Failed to access webcam:", err);
+        });
+    });
+  }, []);
+
+  useEffect(() => {
+    const detect = async () => {
+      if (videoRef.current && canvasRef.current){
+        const video=videoRef.current
+        const canvas=canvasRef.current
+        const displaysize={
+          width:video.videoWidth,
+          height:video.videoHeight
+        }
+        faceapi.matchDimensions(canvas,displaysize)
+        const result = await faceapi.detectAllFaces(
+          video,
+          new faceapi.TinyFaceDetectorOptions()
+        );
+        const resizedDetections=faceapi.resizeResults(result,displaysize)
+
+        canvas.getContext("2d")?.clearRect(0,0,canvas.width,canvas.height);
+        faceapi.draw.drawDetections(canvas,resizedDetections)
+        //nsole.log("Detection Result:", result);
+      }
+    };
+
+    const interval = setInterval(() => {
+      detect();
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+     <div className="relative w-[500px] mt-4">
       <video
         ref={videoRef}
         autoPlay
         playsInline
-        className="w-[500px] rounded shadow-lg"
+        className="rounded shadow-lg w-full"
+      />
+      <canvas
+        ref={canvasRef}
+        className="absolute top-0 left-0 w-full h-full"
       />
     </div>
   );
- }
+}
